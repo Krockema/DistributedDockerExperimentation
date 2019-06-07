a65157dd22b5b759f360dfe21a38f058503b63cf
Martin Krockert <martin.krockert@gmail.com>
2019-06-07 15:34:53 +0200

    Add project files.

---
+++ ExecutorPluginSSOP/ExecutorPluginSimulator/SimulationPlugin.cs
@@ -0,0 +1,33 @@
+﻿using DistributedExperimentation.DataModel;
+using DistributedExperimentation.Experimenter.ExecutorPlugin;
+using System;
+
+namespace ExecutorPluginSimulator
+{
+    public class SimulationPlugin : IExecutorPlugin
+    {
+        public void execute(IExperimentSeries experimentSeries)
+        {
+            var experiment = (IParameterList)experimentSeries.getExperiments();
+            for (uint i = 0; i < experiment.count(); i++)
+            {
+                var parameter = experiment.get(i);
+                if(parameter.getValue().isPrimitive())
+                {
+                    
+                    
+                } else
+                {
+                    for (int j = 0; j < length; j++)
+                    {
+
+                    }
+                }
+                
+            }
+
+
+
+        }
+    }
+}
